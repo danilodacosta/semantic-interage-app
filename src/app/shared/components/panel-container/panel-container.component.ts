6746246d0002b670be2a8df804fd229f150e51d2
danilodacosta <danilo-gc@hotmail.com>
2019-08-27 18:54:42 -0300

    adicionando componente de solicitação de armazem

--- src/app/shared/components/panel-container/panel-container.component.ts
+++ src/app/shared/components/panel-container/panel-container.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import * as $ from 'jquery';
+
 @Component({
   selector: 'app-panel-container',
   templateUrl: './panel-container.component.html',
   styleUrls: ['./panel-container.component.scss']
 })
 export class PanelContainerComponent implements OnInit {
-
   // tslint:disable-next-line: no-input-rename
   @Input('panel-title') panelTitle: string;
   // tslint:disable-next-line: no-input-rename
@@ -18,9 +19,13 @@ export class PanelContainerComponent implements OnInit {
   // tslint:disable-next-line: no-input-rename
   @Input('button-link') buttonLink: string;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    $(document).ready(() => {
+      $('a.fullscreen-link').click(() => {
+        $('div.ibox').toggleClass('ibox-fullscreen');
+      });
+    });
   }
-
 }
